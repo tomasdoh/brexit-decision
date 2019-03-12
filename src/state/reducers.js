95e07eddffd57af42f701c7a8f006b62859fec18
Tomas Doherty <tomasdoh1@gmail.com>
2019-03-12 17:09:32 +0000

    Adding Redux reducers

---
+++ src/state/reducers.js
@@ -0,0 +1,19 @@
+import { gameLoop, MAKE_CHOICE } from "./actions";
+
+export const INITIAL_STATE = {
+  ending: false,
+  ...gameLoop()
+};
+
+export default (state = INITIAL_STATE, { type, ...action }) => {
+  switch (type) {
+    case MAKE_CHOICE:
+      return {
+        ...state,
+        ...action
+      };
+
+default:
+      return state;
+  }
+};
